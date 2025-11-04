66c8e2ae48fb4b52c39139f865dc990384a1ff2b
Jaakko Rajala <jaakko.rajala@tuni.fi>
2025-11-04 18:43:48 +0200

    Update time utility to include hours in formatting

--- lib/utils/numberTimeToMinutesAndSeconds.ts
+++ lib/utils/numberTimeToMinutesAndSeconds.ts
@@ -1,11 +1,17 @@
 /**
- * @description Converts a number time to minutes and seconds. Eg. 60000 -> 1:00
+ * @description Converts a number time to hours, minutes and seconds. Eg. 3661000 -> 1:01:01
  * @param time
  * @returns
  */
 export const numberTimeToMinutesAndSeconds = (time: number) => {
-  if (time < 0) return "0:00";
-  const minutes = Math.floor(time / 60000);
-  const seconds = ((time % 60000) / 1000).toFixed(0);
-  return minutes + ":" + (parseInt(seconds) < 10 ? "0" : "") + seconds;
+  if (time < 0) return "0:00:00";
+  const hours = Math.floor(time / 3600000);
+  const minutes = Math.floor((time % 3600000) / 60000);
+  const seconds = Math.floor((time % 60000) / 1000);
+
+  const formattedHours = hours.toString();
+  const formattedMinutes = minutes < 10 ? "0" + minutes : minutes.toString();
+  const formattedSeconds = seconds < 10 ? "0" + seconds : seconds.toString();
+
+  return formattedHours + ":" + formattedMinutes + ":" + formattedSeconds;
 };
