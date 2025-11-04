f395f84f88e36613bf07e8d6ab42b1c6d77c5ac6
Jaakko Rajala <jaakko.rajala@tuni.fi>
2025-11-04 18:44:36 +0200

    Update Footer: add dark mode toggle and help button, improve layout

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,16 +2,51 @@
 'use client';
 
 import { appVersion } from "@/constants";
+import { setDarkMode } from "@/store/slices/options.slice";
+import HelpModal from "@/components/Help/Help";
+import { useAppDispatch, useAppSelector } from "@/store/hooks/hooks";
+import { Button } from "@/components/ui/button";
+import { Sun } from "lucide-react";
+import PoppingTitle from "@/components/General/PoppingTitle";
+import { useCallback } from "react";
+import InfoInstructions from "../InfoBoard/InfoInstructions";
+import Info from "../InfoBoard/Info";
 
 export const Footer = () => {
+  const dispatch = useAppDispatch();
+  const options = useAppSelector((state) => state.options);
+
+  const toggleDarkMode = useCallback(() => {
+    dispatch(setDarkMode(!options.darkMode));
+  }, [options.darkMode, dispatch]);
+
   return (
     <footer
-      className="text-center p-2 w-full h-fit shrink-0 pointer-events-none text-sm"
+      className="flex flex-row justify-between items-center p-2 w-full h-fit shrink-0 text-sm"
       style={{
         backgroundColor: 'hsl(var(--footer-bg))',
         color: 'hsl(var(--footer-text))',
       }}
     >
+      <div className="flex flex-row gap-2 pointer-events-auto">
+        <PoppingTitle topTitle="Help">
+          <HelpModal />
+        </PoppingTitle>
+        <PoppingTitle topTitle="Toggle Dark Mode">
+          <Button
+            size="icon"
+            variant="ghost"
+            onClick={toggleDarkMode}
+            title="Toggle Dark Mode"
+          >
+            <Sun className="h-5 w-5" />
+          </Button>
+        </PoppingTitle>
+      
+      </div>
+      <InfoInstructions>
+            <Info />
+          </InfoInstructions>
       <p className="text-sm">
         Inspired by
         <a
